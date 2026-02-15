d10fd8c2bba2ed401754a359bfdbb30e7d9d3a2c
Buddhika Madusanka <buddhikadevelopment@gmail.com>
2026-02-15 16:51:19 +0530

    initialized the mistral services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { KnowledgebaseModule } from './knowledgebase/knowledgebase.module';
+import { MistralModule } from './mistral/mistral.module';
 
 @Module({
-  imports: [KnowledgebaseModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    KnowledgebaseModule,
+    MistralModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
